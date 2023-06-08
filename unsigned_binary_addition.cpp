084fc849b50142295ca6359c709eb1eb5c5f92c5
Ajayachaudhary <ajaychy0602@gmail.com>
2023-06-08 19:03:54 +0545

    conversion-and-addition

---
+++ unsigned_binary_addition.cpp
@@ -0,0 +1,45 @@
+//Unsigned binary addition
+#include<iostream>
+using namespace std;
+
+int addb(int [],int [],int [],int);
+
+int main()
+{
+    int n;
+    cout<<"Enter no. of bits::";
+    cin>>n;
+    int *binary1=new int[n], *binary2=new int[n],*sum=new int[n];
+    cout<<"Enter first binary number:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>binary1[i];
+    }
+    cout<<"Enter second binary number:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>binary2[i];
+    }
+
+    int carry=addb(binary1,binary2,sum,n);
+    cout<<"sum:";
+    for(int i=0;i<n;i++)
+    {
+        cout<<sum[i];
+    }
+    cout<<endl<<"carry:"<<carry;
+    return 0;
+}
+
+int addb(int *binary1,int *binary2,int *sum,int n)
+{
+    int carry=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        sum[i]=(binary1[i]+binary2[i]+carry)%2;
+        carry=(binary1[i]+binary2[i]+carry)/2;
+
+    }
+    return carry;
+}
+
---
+++ unsigned_binary_subtraction.cpp
@@ -0,0 +1,85 @@
+//Unsigned binary subtraction
+#include<iostream>
+using namespace std;
+
+int addb(int [],int [],int [],int);
+void get_twos_complement(int [],int [],int);
+int sub(int[],int[],int [],int);
+
+int main()
+{
+    int n;
+    cout<<"Enter no. of bits::";
+    cin>>n;
+    int *minude=new int[n], *subtrahend=new int[n],*diff=new int[n];
+    cout<<"Enter minude number:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>minude[i];
+    }
+    cout<<"Enter subtrahend number:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>subtrahend[i];
+    }
+
+    int carry=sub(minude,subtrahend,diff,n); 
+    cout<<"Difference is:";
+    if(carry==1)
+    {
+        for(int i=0;i<n;i++)
+        {
+            cout<<diff[i];
+        }
+    }
+    else
+    {
+        int *negatives=new int[n];
+        get_twos_complement(diff,negatives,n);
+        cout<<"-";
+        for(int i=0;i<n;i++)
+        {
+            cout<<negatives[i];
+        }
+
+    }
+    cout<<endl;
+    return 0;
+}
+
+int addb(int *binary1,int *binary2,int *sum,int n)
+{
+    int carry=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        sum[i]=(binary1[i]+binary2[i]+carry)%2;
+        carry=(binary1[i]+binary2[i]+carry)/2;
+
+    }
+    return carry;
+}
+
+void get_twos_complement(int *subtrahend,int *twos_complement,int n)
+{
+    int *ones_complement=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        ones_complement[i]=(subtrahend[i]==0)?1:0;
+    }
+    int *one=new int[n];
+    for(int i=0;i<n-1;i++)
+    {
+        one[i]=0;
+    }
+    one[n-1]=1;
+
+    addb(ones_complement,one,twos_complement,n);
+}
+
+int sub(int *minude,int*subtrahend,int*diff,int n)
+{
+    int *twos_complement=new int[n];
+    get_twos_complement(subtrahend,twos_complement,n);
+    return addb(minude,twos_complement,diff,n);
+}
+
